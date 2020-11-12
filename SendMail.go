6267f5b43f11c57a45d8389e8009f610f5130836
DeedWark <deedwark@hotmail.fr>
2020-11-12 18:48:52 +0100

    Add SendMail.go

--- SendMail.go
+++ SendMail.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"crypto/rand"
 	"encoding/base64"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -15,236 +16,318 @@ import (
 	"time"
 )
 
-const (
-	greenTxt = "\033[92m%s\033[00m"
-	cyanTxt  = "\033[96m%s\033[00m"
-	errorTxt = "\033[91m%s\033[00m"
+const ( //COLOR
+	greenTXT  = "\033[92m"   //OK
+	cyanTXT   = "\033[96m"   //INFO
+	yellowTXT = "\033[1;32m" //Others
+	redTXT    = "\033[91m"   //ERROR
+	endTXT    = "\033[00m"   //Ending color
 )
 
-var contentmore string
-var hdate string
-var hfrom string
-var hto string
-var hrid string
-var ctype string
-var b64 string
-var encoding string
+var optSmtpServ string //MX/SMTP flag
+var smtpServ string    //MX/SMTP server
+var port string        //PORT
+var mailFrom string    //MAIL FROM
+var rcptTo string      //RCPT TO
+var hFrom string       //Header From
+var hTo string         //Header To
+var hSub string        //Subject
+var body string        //Body
+var content string     //Content
+var date string        //Date
+var attach string      //Attachment
+var auth bool          //Allow auth (Gmail...)
+var ctype string       //Content-Type
+var encoding string    //Encoding
 
-func main() {
-	//INIT THE KEYBOARD SCANNER
-	scanner := bufio.NewScanner(os.Stdin)
+//OS STDIN SCANNER
+var sc = bufio.NewScanner(os.Stdin)
 
-	//MAIL FROM
-	fmt.Print("MAIL FROM: ")
-	scanner.Scan()
-	mailFrom := scanner.Text()
+//CURRENT DATE
+var cDate = time.Now().Format("Mon, 02 Jan 2006 15:04:05 -0700")
 
-	//RCPT TO
-	fmt.Print("RCPT TO: ")
-	scanner.Scan()
-	rcptTo := scanner.Text()
+//MORE OPTIONS
+var mid string             //Message-ID
+var xmailer string         //X-Mailer
+var charset string         //Encoding
+var promptContent bool     //Write Content with prompt (Allow HTML)
+var htmlFile string        //Read HTML file as Body
+var htmlFileContent []byte //HTML file content
+var txtFile string         //Read txt file content
+var txtFileContent []byte  //Txt file content
+var bs64 bool              //Set base64 encoding
+var xprio string           //X-Priority
+var boundary string        //Custom Boundary
 
-	//Get rcptTo domain
-	cutDomain := strings.Split(rcptTo, "@") //Get only domain
-	domainOnly := cutDomain[len(cutDomain)-1]
+func usage() {
+	fmt.Println(`
+  -s  	         Set SMTP/MX server (default "Autodetect with domain")
+  -p  	         Set TCP Port (default "25/SMTP")
+  -f             Set MAIL FROM (protocolar)
+  -t  	         Set RCPT TO (protocolar)
+--hfrom          Set Header From (ex "Me <go@lang.org>")
+--hto            Set Header To (ex "You <go@pher.org>")
+--subject        Set a subject
+--date           Set a custom date (default "current date")
+--body           Write content to Body
+--attach         Add an attachment/file
+--auth           Enable authentication (Gmail, Outlook...)
+--x-mailer       Set a custom X-Mailer (default "SendMail-Golang v2.0")
+--x-priority     Set a custom X-Priority
+--charset        Set a custom charset (default "UTF-8")
+--html-file      Import a HTML file as body
+--text-file      Import a TXT file as body
+--boundary       Set a custom boundary (default "------=_MIME_BOUNDARY_GOO_LANG--")
+--content-type   Set a custom Content-Type (default "text/plain")
+--base64 Encode  body in base64
+--prompt         Get a prompt to write on your terminal 
+`)
+}
 
-	//Auto detect mx for a domain
+func flags() {
+	//Define FLAGS
+	//    TYPE       VAR      ARGS,DEFAULT    HELP
+	flag.StringVar(&optSmtpServ, "s", "", "Set SMTP/MX server")
+	flag.StringVar(&port, "p", "25", "Set TCP port")
+	flag.StringVar(&mailFrom, "f", "", "Mail From address (MAIL FROM - Protocolar)")
+	flag.StringVar(&rcptTo, "t", "", "Recipient To address (RCPT TO - Protocolar)")
+	flag.StringVar(&hFrom, "hfrom", "", "Set Header From (From:)")
+	flag.StringVar(&hTo, "hto", "", "Set Header To (To:)")
+	flag.StringVar(&hSub, "subject", "", "Set a subject")
+	flag.StringVar(&date, "date", cDate, "Set a custom date")
+	flag.StringVar(&body, "body", "", "Content in body")
+	flag.StringVar(&attach, "attach", "", "Add an attachment")
+	flag.BoolVar(&auth, "auth", false, "Enable authentication (for Gmail/Outlook...)")
+	//MORE OPTIONS
+	//flag.StringVar(&mid, "mid", "<c1882e5b-18b0-3ab5-89a0-ce6a534da8d4@golangmail.this>", "Set a custom Message-ID")
+	flag.StringVar(&xmailer, "x-mailer", "SendMail-Golang v2.0", "Set a custom X-Mailer")
+	flag.StringVar(&charset, "charset", "UTF-8", "Set a charset format")
+	flag.StringVar(&htmlFile, "html-file", "", "Import HTML file as Body")
+	flag.StringVar(&txtFile, "text-file", "", "Import Text file as body")
+	flag.StringVar(&xprio, "x-priority", "1", "Set a custom X-Priority")
+	flag.StringVar(&boundary, "boundary", "------=_MIME_BOUNDARY_GOO_LANG--", "Set a custom Boudnary")
+	flag.StringVar(&ctype, "content-type", "text/plain", "Set a custom Content-Type")
+	flag.BoolVar(&bs64, "base64", false, "Encode body in base64")
+	flag.BoolVar(&promptContent, "body-prompt", false, "Write content with a Prompt")
+
+	flag.Parse()
+}
+
+func sendMail() {
+	flags()
+
+	if rcptTo == "" {
+		fmt.Print("RCPT TO: ")
+		sc.Scan()          //Get
+		rcptTo = sc.Text() //Store os stdin
+	}
+
+	/////////////////////////////////////
+	//      RESOLVE MX WITH DOMAIN     //
+	/////////////////////////////////////
+	cutAddress := strings.Split(rcptTo, "@") //remove @
+	domainOnly := cutAddress[len(cutAddress)-1]
+
 	mxServ := []string{}
 	mxs, _ := net.LookupMX(domainOnly) //Resolve MX
-	var indx string
+
 	if len(mxs) != 0 {
 		for _, mx := range mxs {
-			mxRaw := strings.TrimRight(mx.Host, ".") //cut . at the end
-			mxServ = append(mxServ, mxRaw)           //get MX
+			mxRaw := strings.TrimRight(mx.Host, ".") //Cut ending "."
+			mxServ = append(mxServ, mxRaw)           //Store MX in mxServ list
 		}
 	}
-	cutMx := strings.Join(mxServ, "\n")  //join mx
-	mxList := strings.Split(cutMx, "\n") //slice
-	indx = mxList[0]                     //Get only 1st mx
-	var defMx string
-	if indx != "" {
-		defMx = " (default: " + indx + "): "
+	cutMx := strings.Join(mxServ, "\n")  //join MX with \n
+	mxList := strings.Split(cutMx, "\n") //Slice at \n
+
+	var rMx = mxList[0]
+	if rMx != "" {
+		smtpServ = rMx
 	} else {
-		defMx = ": "
+		fmt.Println("SMTP server not found!" + "\n")
+		fmt.Print("SMTP: ")
+		sc.Scan()            //Get
+		smtpServ = sc.Text() //Store os stdin
 	}
 
-	//MX
-	fmt.Print("SMTP" + defMx)
-	scanner.Scan()
-	smtpServ := scanner.Text()
-	if smtpServ == "" {
-		smtpServ = indx
+	if optSmtpServ != "" {
+		smtpServ = optSmtpServ
 	}
 
-	//Header From
-	fmt.Print("From: ")
-	scanner.Scan()
-	hfrom := scanner.Text()
-
-	//Header To
-	fmt.Print("To: ")
-	scanner.Scan()
-	hto := scanner.Text()
-
-	//Subject
-	fmt.Print("Subject: ")
-	scanner.Scan()
-	hsub := scanner.Text()
-
-	//Current date
-	dt := time.Now()
-	hdate := dt.Format("Mon, 02 Jan 2006 15:04:05 -0700")
-
-	//BODY
-	fmt.Println("CONTENT [. to quit]")
-	block := []string{}
-	for scanner.Scan() {
-		line := scanner.Text()
-		if line != "." {
-			ctype = "text/plain"
-			if line == "<html>" || line == "</html>" {
-				ctype = "text/html"
-				block = append(block, line)
-				continue
-			} else {
-				ctype = "text/plain"
-				block = append(block, line)
-				continue
-			}
-		} else {
-			break
-		}
-	}
-
-	//Join multiline [Body]
-	content := strings.Join(block, "\n")
-
-	//Random ID -> Message-ID
+	////////////////////////////////////////////////////////////////////////
+	//Message-ID -> <c1882e5b-18b0-3ab5-89a0-ce6a534da8d4@golangmail.this>//
+	////////////////////////////////////////////////////////////////////////
 	b := make([]byte, 16)
-	_, err := rand.Read(b)
-	if err != nil {
-		hrid = ""
-	} else {
-		randid := fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
-		hid := "<" + randid + "@golangmail.this>"
-		hrid = hid
-	}
+	rand.Read(b)
+	randomId := fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+	var messageId = "<" + randomId + "@golangmail.this>"
 
-	if ctype == "text/html" {
-		b64 = content
-		encoding = "7bit"
-	} else {
-		fmt.Print("Encode body in base64 [Y/n]: ")
-		scanner.Scan()
-		choice := scanner.Text()
-		switch strings.ToLower(choice) {
-		case "y", "yes":
-			b64 = base64.URLEncoding.EncodeToString([]byte(content))
-			encoding = "base64"
-			ctype = "text/plain"
-		case "n", "no":
-			b64 = content
-			encoding = "7bit"
-		default:
-			b64 = content
-			encoding = "7bit"
-		}
-	}
-
-	//Add Attachment ?
-	fmt.Print("Attachment [Y/n]: ")
-	scanner.Scan()
-	att_ch := scanner.Text()
-
-	var contentmore string
-	var baseContentType string
-
-	switch strings.ToLower(att_ch) {
-
-	case "y", "yes":
-
-		fmt.Print("File: ")
-		scanner.Scan()
-		filePath := scanner.Text()
-		fileraw, err := os.Open(filePath) //Open the attachment file
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		reader := bufio.NewReader(fileraw)       //Init file reader
-		content, _ := ioutil.ReadAll(reader)     //Read and get the file content
-		fileOnly := strings.Split(filePath, "/") //Split at / -> see just below
-		filename := fileOnly[len(fileOnly)-1]    //Get the filename in case of path is like "../dir/dir/image.png"
-
-		//Encode file/attachment in base64
-		encodedFile := base64.StdEncoding.EncodeToString(content)
-		//76 char per line for b64 attach
-		for i, g := 0, len(encodedFile); i < g; i++ {
-			b64buf := bytes.NewBuffer(nil)
-			b64buf.WriteByte(encodedFile[i])
-			if (i+1)%76 == 0 {
-				b64buf.WriteString("\r\n")
+	//////////////////////
+	// CONTENT - PROMPT //
+	//////////////////////
+	if promptContent == true {
+		fmt.Println("CONTENT [. to quit]")
+		block := []string{}
+		for sc.Scan() {
+			line := sc.Text()
+			if line != "." {
+				if line == "<html>" || line == "</html>" {
+					ctype = "text/html"
+					block = append(block, line)
+					continue
+				} else {
+					ctype = "text/plain"
+					block = append(block, line)
+					continue
+				}
+			} else {
+				break
 			}
 		}
-		//All the data to send
-		baseContentType = "Content-Type: multipart/mixed; boundary=\"----=_MIME_BOUNDARY_GOO_LANG\"" + "\r\n\r\n" +
-			"------=_MIME_BOUNDARY_GOO_LANG" + "\r\n" +
-			"Content-Type: " + ctype + "; charset=\"UTF-8\"" + "\r\n" +
+		body = strings.Join(block, "\n") //Join multiline content
+	}
+
+	/////////////
+	// Charset //
+	/////////////
+	if charset != "" {
+		switch strings.ToLower(charset) {
+		case "utf-8", "utf8":
+			charset = "UTF-8"
+		case "usascii", "us", "us-ascii":
+			charset = "US-ASCII"
+		case "quoted-printable", "qp", "quoted", "printable":
+			charset = "quoted-printable"
+		default:
+			charset = "UTF-8"
+		}
+	}
+
+	//////////////////////
+	// HTML File Import //
+	//////////////////////
+	if htmlFile != "" {
+		htmlFileRaw, err := os.Open(htmlFile) //Open the HTML file
+		if err != nil {
+			fmt.Println(redTXT + "Cannot open HTML file" + endTXT)
+			log.Fatalln(err)
+		}
+
+		reader := bufio.NewReader(htmlFileRaw)      //Init the file reader
+		htmlFileContent, _ = ioutil.ReadAll(reader) //Read and get HTML file content
+		body = string(htmlFileContent)
+		ctype = "text/html"
+	}
+
+	//////////////////////
+	// TEXT File Import //
+	//////////////////////
+	if txtFile != "" {
+		txtFileRaw, err := os.Open(txtFile) //Open txt file
+		if err != nil {
+			fmt.Println(redTXT + "Cannot open TEXT file" + endTXT)
+			log.Fatalln(err)
+		}
+
+		reader := bufio.NewReader(txtFileRaw)      //Init the file reader
+		txtFileContent, _ = ioutil.ReadAll(reader) //Read and get HTML file content
+		body = string(txtFileContent)
+		ctype = "text/plain"
+	}
+
+	///////////////////////////////
+	// Content-Transfer Encoding //
+	///////////////////////////////
+	if bs64 == true {
+		if ctype != "text/html" {
+			encoding = "base64"
+			body = base64.URLEncoding.EncodeToString([]byte(body))
+		} else {
+			encoding = "7bit"
+		}
+	} else {
+		encoding = "7bit"
+	}
+
+	////////////////
+	// Attachment //
+	////////////////
+	if attach != "" {
+		fileRaw, err := os.Open(attach) //Open file
+		if err != nil {
+			log.Fatalln(redTXT + "Cannot open the File as attachment" + endTXT)
+		}
+
+		fileReader := bufio.NewReader(fileRaw)       //Init the file reader
+		contentFile, _ := ioutil.ReadAll(fileReader) //Read and get content file
+		fileOnly := strings.Split(attach, "/")       //Split at "/" in case of Unix Path
+		filename := fileOnly[len(fileOnly)-1]        //Get only filename
+
+		//
+		//ENCODE FILE/ATTACHMENT IN BASE64
+		//
+		encodedFile := base64.StdEncoding.EncodeToString(contentFile)
+
+		content = "Content-Type: multipart/mixed; boundary=" + boundary + "\r\n\r\n" +
+			boundary + "\r\n" +
+			"Content-Type: " + ctype + "; charset=" + charset + "\r\n" +
 			"Content-Transfer-Encoding: " + encoding + "\r\n" +
-			"\r\n" + b64 + "\r\n" +
-			"------=_MIME_BOUNDARY_GOO_LANG" + "\r\n" +
+			"\r\n" + body + "\r\n" +
+			boundary + "\r\n" +
 			"Content-Type: application/octet-stream; name=\"" + filename + "\"" + "\r\n" +
 			"Content-Description: " + filename + "\r\n" +
 			"Content-Disposition: attachment; filename=\"" + filename + "\"" + "\r\n" +
 			"Content-Transfer-Encoding: base64" + "\r\n\r\n" +
-			encodedFile + "\r\n\r\n" + "------=_MIME_BOUNDARY_GOO_LANG--"
-
-	case "n", "no":
-
-		//All the data without attachment
-		baseContentType = "Content-Type: " + ctype + "; charset=\"UTF-8\"" + "\r\n" +
+			encodedFile + "\r\n\r\n" + boundary
+	} else {
+		content = "Content-Type: " + ctype + "; charset=" + charset + "\r\n" +
 			"Content-Transfer-Encoding: " + encoding + "\r\n" +
-			"\r\n" + b64
+			"\r\n" + body
 	}
 
-	contentmore = "Date: " + hdate + "\r\n" +
-		"From: " + hfrom + "\r\n" +
-		"To: " + hto + "\r\n" +
-		"Subject: " + hsub + "\r\n" +
-		"Message-ID: " + hrid + "\r\n" +
-		"X-Mailer: SendMail-Golang v1.0" + "\r\n" +
+	var baseContent string = "Date: " + date + "\r\n" +
+		"From: " + hFrom + "\r\n" +
+		"To: " + hTo + "\r\n" +
+		"Subject: " + hSub + "\r\n" +
+		"Message-ID: " + messageId + "\r\n" +
+		"X-Mailer: " + xmailer + "\r\n" +
+		"X-Priority: " + xprio + "\r\n" +
 		"MIME-Version: 1.0" + "\r\n" +
-		baseContentType
+		content
 
-	//Print Overview
-	fmt.Println("\r\n" + "---------------Overview---------------" + "\n" + contentmore + "\n" + "--------------------------------------")
+	fmt.Println("\r\n" + yellowTXT + "---------------Overview---------------" + endTXT + "\n" + baseContent + "\n" + yellowTXT + "--------------------------------------" + endTXT)
+	fmt.Println(cyanTXT + "Sending in progress... please wait!" + "\n" + endTXT)
 
-	fmt.Printf("\n"+cyanTxt, "Sending in progress... please wait!"+"\n")
-
-	// Connect to SMTP server
-	mx, err := smtp.Dial(smtpServ + ":25")
+	//
+	//Connect to SMTP serv
+	mx, err := smtp.Dial(smtpServ + ":" + port)
 	if err != nil {
-		fmt.Printf("\n"+errorTxt, "ERROR: Cannot connect to "+smtpServ+":25"+"\n")
-		log.Fatal(err)
+		fmt.Println(redTXT + "Error: Cannot connect to " + smtpServ + ":" + port + "\n" + endTXT)
+		log.Fatalln(err)
 	}
 	defer mx.Close()
 
-	// Set MailFrom and RcptTo
+	//
+	//Set MailFrom and RcptTo
 	mx.Mail(mailFrom)
 	mx.Rcpt(rcptTo)
 
-	// Send email body
+	//
+	//Send email body
 	mxc, err := mx.Data()
 	if err != nil {
-		fmt.Printf("\n"+errorTxt, "Body Error!"+"\n")
-		log.Fatal(err)
+		fmt.Println(redTXT + "Error: " + endTXT)
+		log.Fatalln(err)
 	}
 	defer mxc.Close()
-	buf := bytes.NewBufferString(contentmore)
+	buf := bytes.NewBufferString(body)
 	if _, err = buf.WriteTo(mxc); err != nil {
-		fmt.Println(errorTxt, "500: Mail not sent!")
-		log.Fatal(err)
+		fmt.Println(redTXT + "500: Mail not sent!" + endTXT)
 	} else {
-		fmt.Printf("\n"+greenTxt, "250: Mail sent!  -->  Message-ID: "+hrid+"\r\n")
+		fmt.Println(greenTXT + "250: Mail sent!  -->  Message-ID: " + messageId + "\r\n")
 	}
+}
+
+func main() {
+	sendMail()
 }
